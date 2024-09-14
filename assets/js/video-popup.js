abc856cca4fdb2e85f85ea3708df2ad0ff43cd70
Xiaojiang Chen <xjchen@nwu.edu.cn>
2024-09-14 20:27:58 +0800

    Update video-popup.js

--- assets/js/video-popup.js
+++ assets/js/video-popup.js
@@ -1,9 +1,6 @@
-// video-popup.js
+// /assets/js/video-popup.js
 function openVideoPopup(videoSrc) {
-    // 打开一个固定大小的新窗口
     const videoWindow = window.open("", "VideoPopup", "width=600,height=400");
-
-    // 在新窗口中插入 HTML 代码，包括视频标签
     videoWindow.document.write(`
         <html>
         <head>
@@ -11,7 +8,7 @@ function openVideoPopup(videoSrc) {
         </head>
         <body style="margin:0; padding:0; display:flex; justify-content:center; align-items:center;">
             <video width="600" height="400" controls autoplay>
-                <source src="` + videoSrc + `" type="video/mp4">
+                <source src="${videoSrc}" type="video/mp4">
                 您的浏览器不支持 HTML5 视频播放。
             </video>
         </body>
